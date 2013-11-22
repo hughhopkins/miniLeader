80dde550726de2feb00d9ce7c00654482d9279c7
Hugh Hopkins <hugh.hopkins@gmail.com>
2013-11-22 20:36:44 +0000

    consolelog
    added console.log to the js to see what is actually going on

--- client/client.js
+++ client/client.js
@@ -1,12 +1,29 @@
 Template.leaderboard.players = function () {
   return Players.find({}, {sort: {score: -1, name: 1}});
+  console.log("players.find");
 };
 
 Template.leaderboard.selected_name = function () {
   var player = Players.findOne(Session.get("selected_player"));
   return player && player.name;
+  console.log("findOne");
 };
 
 Template.player.selected = function () {
   return Session.equals("selected_player", this._id) ? "selected" : '';
+  console.log("session.equals");
 };
+
+Template.leaderboard.events({
+  'click input.inc': function () {
+    Players.update(Session.get("selected_player"), {$inc: {score: 5}});
+    console.log("upvote");
+  }
+});
+
+Template.player.events({
+  'click': function () {
+    Session.set("selected_player", this._id);
+    console.log("session.set");
+  }
+});
